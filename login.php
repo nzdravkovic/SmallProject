4012ca090f453c20faeb6e3ce4749d943fa7cec0
geega17 <32374532+geega17@users.noreply.github.com>
2018-09-10 19:10:05 -0400

    Update login.php
    added input validation

--- login.php
+++ login.php
@@ -1,12 +1,34 @@
 <?php 
 
+	
+	function userfilter($string) 
+	{
+		return preg_replace("/[^A-Za-z0-9]/","",$string);
+	}
+	
+	function passfilter($string) 
+	{
+		return preg_replace("/[^A-Za-z0-9!@#$%^&*]/","",$string);
+	}
+
+	
+	//The JSON that this function receives is from index.js and looks like this:
+	//var jsonPayload = '{"Log" : "' + login + '", "password" : "' + hashedPassword + '"}';
+
 	// Put json into array
 	$inData = getRequestInfo();
-
+	
+	$cleanData = array(
+    'Log'   => userfilter($inData["Log"],
+    'password' => passfilter($inData["password"])
+	);
+	
+	
+	
 	// Assign variables json strings 
 	// reference the json in the javascript code 
-	$login = $inData["Log"];
-	$pw = $inData["password"];
+	$login = $cleanData["Log"];
+	$pw = $cleanData["password"];
 
 	// Info for server and database access -- change values to server info on aws
 	$username = 'root';
@@ -16,6 +38,28 @@
 
 	// Connect to remote database on server
 	$conn = new mysqli($server, $username, $password, $database);
+
+	//note: this if has one equals because we're checking to see if the function returned with an error or not
+	if($sql = $mysqli->prepare('SELECT * FROM users WHERE username = ? and password = ?')):
+		$sql->bind_param('ss', $login, $pwd);
+		
+		if($sql->execute()):
+
+			$sql->fetch();
+
+			if($sql->num_rows > 0):
+				
+				//successful login!
+			else:
+				//nooooope
+				return;
+			endif;
+			
+		endif;
+			$sql->close();
+			//what does this return?
+			return json_encode($ret);
+	endif;
 
 	$id = 0;
 	$firstName = "";
@@ -27,28 +71,9 @@
 	{
 		returnWithError($conn->connect_error);
 	}
-	// Whoever works on this can probably change this whole piece of code -- not sure if it works 100%
-	else
-	{
-		// Check if the password matches the hashed password
-		if(password_verify($pw, $row[2]))
-		{
-			$sql = "select * from users where username = '$login'";
-			$result = $conn->query($query);
 
-			if($result->num_rows > 0)
-			{
-				$row = $result->fetch_assoc();
-				$firstName = $row["firstName"];
-				$lastName = $row["lastName"];
-				$id = $row["userID"];
-			}
-			else
-			{
-				returnWithError("No records found");
-			}
-		}
-		
+	else
+	{		
 		$conn->close();
 	}
 
